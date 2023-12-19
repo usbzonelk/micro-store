502a48b396ff70e7713dcf7bcc36b55c16c9f1fa
Bhashith Udawaththa <bhashith.me@gmail.com>
2023-12-19 10:00:04 +0530

    Added IUnitOfWork

---
+++ ProductService/Repository/IUnitOfWork.cs
@@ -0,0 +1,10 @@
+using System.Linq.Expressions;
+using Microsoft.EntityFrameworkCore;
+using ProductService.Models;
+
+namespace ProductService.Repository;
+
+public interface IUnitOfWork
+{
+    IProductRepository ProductRepository { get; set; }
+}
