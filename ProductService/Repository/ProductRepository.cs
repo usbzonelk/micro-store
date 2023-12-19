56db797f523c51a93527787b04f6a1cfce8f4452
Bhashith Udawaththa <bhashith.me@gmail.com>
2023-12-19 18:58:40 +0530

    Updated Product Repository

--- ProductService/Repository/ProductRepository.cs
+++ ProductService/Repository/ProductRepository.cs
@@ -7,38 +7,12 @@ using ProductService.Models;
 
 namespace ProductService.Repository;
 
-public class ProductRepository : IProductRepository
+public class ProductRepository : Repository<Product>, IProductRepository
 {
-    private readonly ProductServiceDBContext _db;
-    internal DbSet<Product> dbSet;
-
-    public ProductRepository(ProductServiceDBContext db)
+    private ProductServiceDBContext _db;
+    public ProductRepository(ProductServiceDBContext db) : base(db)
     {
         _db = db;
-        dbSet = _db.Set<Product>();
-    }
-    public void Add(Product product)
-    {
-        this.dbSet.Add(product);
     }
 
-    public Product Get(Expression<Func<Product, bool>> find)
-    {
-        return dbSet.Where(find).FirstOrDefault();
-    }
-
-    public IEnumerable<Product> GetAll()
-    {
-        return dbSet.ToList();
-    }
-
-    public IEnumerable<Product> GetMany(Expression<Func<Product, bool>> find)
-    {
-        return dbSet.Where(find).ToList();
-    }
-
-    public void Remove(Product product)
-    {
-        dbSet.Remove(product);
-    }
 }
