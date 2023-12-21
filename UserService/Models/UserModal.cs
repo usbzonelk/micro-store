72140bf7922728f27f346b61d887d5b1559cc57e
Bhashith Udawaththa <bhashith.me@gmail.com>
2023-12-21 17:36:05 +0530

    Updated modal : UserService

--- UserService/Models/UserModal.cs
+++ UserService/Models/UserModal.cs
@@ -33,9 +33,7 @@ namespace UserService.Models
         public string FirstName { get; set; }
 
         public string LastName { get; set; }
-
-        [DataType(DataType.Date)]
-
+        
         public string PhoneNumber { get; set; }
 
         // Address information
